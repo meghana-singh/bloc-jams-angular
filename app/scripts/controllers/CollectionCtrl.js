15ab40e179c3113163955343f34c2e8ad7f95d30
meghana singh <meghana1602@gmail.com>
2016-10-10 17:41:37 -0700

    Checkpoint:6 Services

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,15 +1,11 @@
 //Controller for Collections View
 (function() {
-     function CollectionCtrl() {
-        this.albums = [];
-        for (var i=0; i < 12; i++) {
-         this.albums.push(angular.copy(albumPicasso));
-        // this.albums.push(angular.copy(albumMarconi));
-        // this.albums.push(angular.copy(albumAdele));
-        }
+     function CollectionCtrl(Fixtures) {
+         this.albums = Fixtures.getCollection(12);
      }
  
      angular
          .module('blocJams')
-         .controller('CollectionCtrl', CollectionCtrl);
+         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
+         
  })();
--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,12 @@
 (function() {
     
-    function AlbumCtrl() {
-          this.album = {};
-          this.albumData = angular.copy(albumPicasso);
+    function AlbumCtrl(Fixtures) {
+          this.albumData = {};
+          this.albumData = Fixtures.getAlbum();
     }
     
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 
 })();
